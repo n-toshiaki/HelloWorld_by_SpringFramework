e1c7c63d0240e00e5abb9ddcf8c793287d9b38c3
n-toshiaki <biohazard.tn1992@gmail.com>
2023-09-21 21:56:07 +0900

    ブラウザに表示されるものをHello Worldから

---
+++ src/main/java/com/hello/hello/Food.java
@@ -0,0 +1,27 @@
+package com.hello.hello;
+
+public class Food {
+    private String name;
+
+    private String materials;
+
+    private String country;
+
+    public Food(String name, String materials, String country) {
+        this.name = name;
+        this.materials = materials;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMaterials() {
+        return materials;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
---
+++ src/main/java/com/hello/hello/FoodController.java
@@ -0,0 +1,20 @@
+package com.hello.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+public class FoodController {
+
+    @GetMapping("/hello")
+    public List<Food> food() {
+        List<Food> foods = List.of(
+                new Food("hamburger","meat&bread&cheese","USA"),
+                new Food("pizza","tomato&bread&cheese","Italy"),
+                new Food("sushi","fish&rice","Japan"),
+                new Food("curry","spices","India")
+        );
+        return foods;
+    }
+}
