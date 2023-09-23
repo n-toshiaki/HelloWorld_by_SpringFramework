1fdbace4440a8281cb0ed0379aabcff9c531536f
n-toshiaki <biohazard.tn1992@gmail.com>
2023-09-23 10:23:56 +0900

    リストに中華料理を追加しました

--- src/main/java/com/hello/hello/FoodController.java
+++ src/main/java/com/hello/hello/FoodController.java
@@ -4,16 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 @RestController
 public class FoodController {
 
     @GetMapping("/hello")
     public List<Food> food() {
         List<Food> foods = List.of(
-                new Food("hamburger","meat&bread&cheese","USA"),
-                new Food("pizza","tomato&bread&cheese","Italy"),
-                new Food("sushi","fish&rice","Japan"),
-                new Food("curry","spices&vegetables","India")
+                new Food("hamburger", "meat&bread&cheese", "USA"),
+                new Food("pizza", "tomato&bread&cheese", "Italy"),
+                new Food("sushi", "fish&rice", "Japan"),
+                new Food("curry", "spices&vegetables", "India"),
+                new Food("Peking duck", "duck&vegetables", "China")
         );
         return foods;
     }
